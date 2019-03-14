3d2e04af3886f5dadf9e58615706a9953b70c29d
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-03-14 15:12:03 -0700

    Add files via upload

---
+++ bool.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+    class Program
+    {
+    static void Main()
+    {
+        int i = 0;
+        do
+        {
+            int j = 0;
+
+            while (j < 3) 
+            {
+                Console.WriteLine("Value of j: {0}", j);
+
+                j++;
+
+            } 
+            i++;
+            Console.WriteLine("Value of i: {0}", i);
+
+        } while (i < 3) ;
+        Console.ReadLine();
+
+    }
+}
+
