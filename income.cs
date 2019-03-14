85bb7ca6240ca4812326cd3b3ddfa2fc563df5ec
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-03-14 12:42:22 -0700

    Add files via upload

---
+++ income.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Anonymous Income Comparison Program");
+        Console.WriteLine("Person 1");
+        Console.WriteLine("Hourly Rate?");
+        decimal hourlyRate1 = decimal.Parse(Console.ReadLine());
+        Console.WriteLine("Hours worked per week?");
+        decimal hoursWorked1 = decimal.Parse(Console.ReadLine());
+        Console.WriteLine("Person 2");
+        Console.WriteLine("Hourly Rate?");
+        decimal hourlyRate2 = decimal.Parse(Console.ReadLine());
+        Console.WriteLine("Hours worked per week?");
+        decimal hoursWorked2 = decimal.Parse(Console.ReadLine());
+        Console.WriteLine("Does Person 1 make more money than Person 2?");
+        decimal total1 = (hourlyRate1 * hoursWorked1);
+        decimal total2 = (hourlyRate2 * hoursWorked2);
+        if (total1 > total2)
+        {
+            Console.WriteLine("True.");
+        }
+        else
+        {
+            Console.WriteLine("False.");
+        }
+        Console.ReadLine();
+
+    }
+    }
+
