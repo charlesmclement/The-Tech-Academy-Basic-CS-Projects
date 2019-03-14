5468b393acd30dbe6ec61fff100a0037592556b2
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-03-14 00:41:01 -0700

    Add files via upload

---
+++ dailyreport.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("The Tech Academy");
+        Console.WriteLine("Student Daily Report");
+        Console.WriteLine("");
+        Console.WriteLine("What course are you on?");
+        string currentCourse = Console.ReadLine();
+        Console.WriteLine("What page number?");
+        byte currentPage = byte.Parse(Console.ReadLine());
+        Console.WriteLine("Do you need any help with anything? Please answer \"true\" or \"false.\"");
+        bool needHelp = bool.Parse(Console.ReadLine());
+        Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+        string Experience = Console.ReadLine();
+        Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+        string Feedback = Console.ReadLine();
+        Console.WriteLine("How many hours did you study today?");
+        byte hoursWorked = byte.Parse(Console.ReadLine());
+        Console.WriteLine("");
+        Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+        Console.Read();
+    }
+    }
