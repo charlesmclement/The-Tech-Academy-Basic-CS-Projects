77b107f53b08de4175c0307479cb3718ba0bca2b
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-03-15 09:17:28 -0700

    Add files via upload

---
+++ StringBuilderPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderPractice
+{
+    class Program
+    {
+        static void Main()
+        {
+            string sig1 = "have ";
+            string sig2 = "a good ";
+            string sig3 = "one!";
+            string sig = (sig1 + sig2 + sig3);
+            sig = sig.ToUpper();
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Hello World,\n");
+            sb.Append("I am practicing using StringBuilder.\n");
+            sb.Append("It is used to manipulate strings easily.\n");
+            sb.Append(sig);
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
