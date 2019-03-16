d19cd6e905b895380045dce4ea7426b9725133b9
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-03-16 00:03:09 -0700

    Add files via upload

---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int i = int.Parse(Console.ReadLine());
+            int addResults = calculator.Adding(i);
+            int multResults = calculator.Multiplying(i);
+            int subResults = calculator.Subtracting(i);
+            Console.WriteLine(i + " + 50 = " + addResults);
+            Console.WriteLine(i + " * 50 = " + multResults);
+            Console.WriteLine(i + " - 50 = " + subResults);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ calculator/calculator/calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class calculator
+    {
+        public static int Adding(int args)
+        {
+            int Addition = args + 50;
+            return Addition;
+        }
+        public static int Multiplying(int args)
+        {
+            int Multiplication = args * 50;
+            return Multiplication;
+        }
+        public static int Subtracting(int args)
+        {
+            int Subtraction = args - 50;
+            return Subtraction;
+        }
+    }
+}
