1bab3c4bc8fed0af454a7ce6b7854b0514dc0368
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-03-16 14:40:07 -0700

    Add files via upload

---
+++ half/half/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace half
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            intTry:
+            string input = Console.ReadLine();
+            Console.WriteLine("Enter a number:");
+            try
+            {
+                int i = int.Parse(input);
+                half.Half(i, out i);
+            }
+            catch
+            {
+                goto decTry;
+            }
+            decTry:
+            try
+            {
+                decimal i = decimal.Parse(Console.ReadLine());
+                half.Half(i, out i);
+            }
+            catch (FormatException)
+            {
+                goto intTry;
+            }
+            finally {
+                Console.ReadLine();
+            }
+        }
+    }
+}
