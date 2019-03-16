dfc59c2d0e4ce043e35a9fca294edc965bcd0a64
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-03-15 22:38:13 -0700

    Add files via upload

---
+++ dividebyzero/dividebyzero/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dividebyzero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> Numbers = new List<int> { 2, 3, 5, 7, 11 };
+                Console.WriteLine("Enter a number:");
+                int i = int.Parse(Console.ReadLine());
+                foreach (int Number in Numbers)
+                {
+                    Console.WriteLine(Number / i);
+                }
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("\n...finally");
+                Console.ReadLine();
+            }
+        }
+    }
+}
