3f954426405a0f0a61d72292ab0343cb6de77ca2
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-03-19 06:48:07 -0700

    Add files via upload

---
+++ half/half/half/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace half
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            string input = Console.ReadLine();
+            try
+            {
+                decimal i = decimal.Parse(input);
+                half.Half(i, out i);
+            }
+            catch
+            {
+                decimal i = decimal.Parse(input);
+                half.Half(i, out i);
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ half/half/half/half.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace half
+{
+    class half
+    {
+        public static void Half(int input,out int number)
+        {
+            number = (input / 2);
+            Console.WriteLine("Half of " + input + " is " + number);
+
+        }
+
+        public static void Half(decimal input, out decimal number)
+        {
+            number = (input / 2m);
+            Console.WriteLine("Half of " + input + " is " + number);
+        }
+
+    }
+}
