540620b4835a30976ad2372f2553ef47a7fcc832
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-03-22 10:40:38 -0700

    Add files via upload

---
+++ sampleStudent/sampleStudent/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampleStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Employee sampleStudent = new Employee() { FirstName = "Sample", LastName = "Student" };
+            sampleStudent.SayName();
+        }
+    }
+}
+
