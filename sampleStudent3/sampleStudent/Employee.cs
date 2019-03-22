4c710175e1344aedc7386146477eaa569affe692
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-03-22 12:12:38 -0700

    Add files via upload

---
+++ sampleStudent3/sampleStudent/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampleStudent
+{
+    class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+            Console.ReadLine();
+        }
+        public void Quit()
+        {
+            Console.WriteLine(FirstName + " " + LastName + " has quit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sampleStudent3/sampleStudent/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampleStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            IQuittable sampleStudent = new Employee() { FirstName = "Sample", LastName = "Student" };
+            sampleStudent.Quit();
+        }
+    }
+}
+
