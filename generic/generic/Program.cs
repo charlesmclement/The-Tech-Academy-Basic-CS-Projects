a1cf64870f10583ea8fe399082ad320c14201756
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-03-26 12:37:31 -0700

    Add files via upload

---
+++ generic/generic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generic
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee<string> Stringz = new Employee<string>();
+            Employee<int> Intz = new Employee<int>();
+            List<string> Strings = new List<string> { "1", "2", "3", "4", "5" };
+            List<int> Ints = new List<int> { 1, 2, 3, 4, 5 };
+            Stringz.Things = Strings;
+            Intz.Things = Ints;
+
+            foreach (int thing in Intz.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            foreach (string thing in Stringz.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            Console.ReadLine();
+        }
+    }
+}
