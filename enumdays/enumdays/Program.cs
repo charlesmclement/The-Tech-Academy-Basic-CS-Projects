e7ec043e69eedd9776bff56f47108ba1c9a6b327
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-03-26 21:54:29 -0700

    Add files via upload

---
+++ enumdays/enumdays/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumdays
+{
+    class Program
+    {
+        enum Days { sunday, monday = 1, tuesday = 2, wednesday = 3, thursday = 4, friday = 5, saturday = 6 };
+
+        static void Main(string[] args)
+        {
+        dayInput:
+            System.Console.WriteLine("input a day");
+            string input = System.Console.ReadLine().ToLower();
+            Days myDays = new Days();
+
+
+
+            if (Enum.TryParse(input, out myDays))
+            {
+                switch (myDays)
+                {
+                    case Days.sunday:
+                        Console.WriteLine("It is Sunday");
+                        break;
+                    case Days.monday:
+                        Console.WriteLine("It is Monday");
+                        break;
+                    case Days.tuesday:
+                        Console.WriteLine("It is Tuesday");
+                        break;
+                    case Days.wednesday:
+                        Console.WriteLine("It is Wednesday");
+                        break;
+                    case Days.thursday:
+                        Console.WriteLine("It is Thursday");
+                        break;
+                    case Days.friday:
+                        Console.WriteLine("It is Friday");
+                        break;
+                    case Days.saturday:
+                        Console.WriteLine("It is Saturday");
+                        break;
+                }
+            
+            }
+            else
+            {
+                Console.WriteLine("Enter an actual day");
+                goto dayInput;
+            }
+
+
+            Console.ReadLine();
+        }
+
+
+
+    }
+}
+
