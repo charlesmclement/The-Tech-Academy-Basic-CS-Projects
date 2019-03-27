57fe57deb02c4ed48235b5ca40394e836afce822
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-03-26 22:07:06 -0700

    Add files via upload

--- enumdays/enumdays/Program.cs
+++ enumdays/enumdays/Program.cs
@@ -8,7 +8,7 @@ namespace enumdays
 {
     class Program
     {
-        enum Days { sunday, monday, tuesday, wednesday, thursday, friday, saturday};
+        enum Days { sunday, monday, tuesday, wednesday, thursday, friday, saturday };
 
         static void Main(string[] args)
         {
@@ -17,49 +17,39 @@ namespace enumdays
             string input = System.Console.ReadLine().ToLower();
             Days myDays = new Days();
 
-
-            try
+            if (Enum.TryParse(input, out myDays))
             {
-                if (Enum.TryParse(input, out myDays))
+                switch (myDays)
                 {
-                    switch (myDays)
-                    {
-                        case Days.sunday:
-                            Console.WriteLine("It is Sunday");
-                            break;
-                        case Days.monday:
-                            Console.WriteLine("It is Monday");
-                            break;
-                        case Days.tuesday:
-                            Console.WriteLine("It is Tuesday");
-                            break;
-                        case Days.wednesday:
-                            Console.WriteLine("It is Wednesday");
-                            break;
-                        case Days.thursday:
-                            Console.WriteLine("It is Thursday");
-                            break;
-                        case Days.friday:
-                            Console.WriteLine("It is Friday");
-                            break;
-                        case Days.saturday:
-                            Console.WriteLine("It is Saturday");
-                            break;
-                    }
+                    case Days.sunday:
+                        Console.WriteLine("It is Sunday");
+                        break;
+                    case Days.monday:
+                        Console.WriteLine("It is Monday");
+                        break;
+                    case Days.tuesday:
+                        Console.WriteLine("It is Tuesday");
+                        break;
+                    case Days.wednesday:
+                        Console.WriteLine("It is Wednesday");
+                        break;
+                    case Days.thursday:
+                        Console.WriteLine("It is Thursday");
+                        break;
+                    case Days.friday:
+                        Console.WriteLine("It is Friday");
+                        break;
+                    case Days.saturday:
+                        Console.WriteLine("It is Saturday");
+                        break;
+                }
 
-                }
-                else
-                {
-                    Console.WriteLine("Enter an actual day");
-                    goto dayInput;
-                }
             }
-            catch
+            else
             {
                 Console.WriteLine("Enter an actual day");
                 goto dayInput;
             }
-
 
             Console.ReadLine();
         }
