54f846d7e7c1e8a45a81f069354a03aab8f8aa66
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-03-27 22:39:37 -0700

    Add files via upload

---
+++ numStruct/numStruct/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numStruct
+{
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number Num = new Number();
+            Num.Amount = 3.3m;
+            Console.WriteLine(Num.Amount);
+            Console.ReadLine();
+        }
+    }
+}
