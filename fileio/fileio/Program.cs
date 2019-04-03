c6df441964405e3b2efaa2a2cf4b734b25d31224
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-04-02 22:44:15 -0700

    Add files via upload

---
+++ fileio/fileio/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace fileio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            string input = Console.ReadLine();
+            File.WriteAllText(@"C:\logs\log.txt", input);
+            string output = File.ReadAllText(@"C:\logs\log.txt");
+            Console.WriteLine("You chose " + output);
+            Console.ReadLine();
+
+        }
+    }
+}
