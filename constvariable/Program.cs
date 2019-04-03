d11093222c7459ef88d4cb1a1d83223e8cb0c699
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-04-03 11:46:31 -0700

    Update Program.cs

--- constvariable/Program.cs
+++ constvariable/Program.cs
@@ -21,50 +21,50 @@ namespace constvariable
             string guild = "";
             string quit = "no";
 
-                Console.WriteLine("Would you like to be a medic or soldier? \n(type soldier or medic, optional)");
-                guild = Console.ReadLine().ToLower();
-                switch (guild)
-                {
-                    case "medic":
-                        Console.WriteLine("You chose Medic!\n");
-                        userid = Guid.NewGuid().ToString();
-                        Medic medic = new Medic(userid, username, Medic.Job);
-                        People.Add(medic);
-                        break;
-                    case "soldier":
-                        Console.WriteLine("You chose Soldier!\n");
-                        userid = Guid.NewGuid().ToString();
-                        Soldier soldier = new Soldier(userid, username, Soldier.Job);
-                        People.Add(soldier);
-                        break;
-                    default:
-                        Console.WriteLine("You're just an average person.\n");
-                        userid = Guid.NewGuid().ToString();
-                        Person person = new Person(userid, username, "none");
-                        People.Add(person);
-                        break;
-                }
-                foreach (Person person in People)
-                {
-                    Console.WriteLine("{0}: {1}", person.Name, person.Id);
-                    person.Talk();
-                }
-                anotherOne:
-                Console.WriteLine("\nMake another? y/n");
-                quit = Console.ReadLine().ToLower();
-                if (quit == "yes" || quit == "y")
-                {
-                    goto creation;
-                }
-                else if (quit == "no" || quit == "n")
-                {
-                    Environment.Exit(0);
-                }
-                else
-                {
-                    goto anotherOne;
-                }
-            
+            Console.WriteLine("Would you like to be a medic or soldier? \n(type soldier or medic, optional)");
+            guild = Console.ReadLine().ToLower();
+            switch (guild)
+            {
+                case "medic":
+                    Console.WriteLine("You chose Medic!\n");
+                    userid = Guid.NewGuid().ToString();
+                    Medic medic = new Medic(userid, username, Medic.Job);
+                    People.Add(medic);
+                    break;
+                case "soldier":
+                    Console.WriteLine("You chose Soldier!\n");
+                    userid = Guid.NewGuid().ToString();
+                    Soldier soldier = new Soldier(userid, username, Soldier.Job);
+                    People.Add(soldier);
+                    break;
+                default:
+                    Console.WriteLine("You're just an average person.\n");
+                    userid = Guid.NewGuid().ToString();
+                    Person person = new Person(userid, username, "none");
+                    People.Add(person);
+                    break;
+            }
+            foreach (Person person in People)
+            {
+                Console.WriteLine("{0}: {1}", person.Name, person.Id);
+                person.Talk();
+            }
+            anotherOne:
+            Console.WriteLine("\nMake another? y/n");
+            quit = Console.ReadLine().ToLower();
+            if (quit == "yes" || quit == "y")
+            {
+                goto creation;
+            }
+            else if (quit == "no" || quit == "n")
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                goto anotherOne;
+            }
+
 
             Console.ReadLine();
         }
