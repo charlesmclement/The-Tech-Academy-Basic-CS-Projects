5ec8620719c987c30c5881ea30a9d62d974d0d47
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-04-02 23:01:57 -0700

    Add files via upload

---
+++ datetime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It is now: " + DateTime.Now + "\nEnter a number: ");
+            double input;
+            input:
+            try
+            {
+                input = double.Parse(Console.ReadLine());
+                Console.WriteLine("It will be " + DateTime.Now.AddHours(input) + " {0} hours from now", input);
+                Console.ReadLine();
+            }
+            catch
+            {
+                goto input;
+            }
+        }
+    }
+}
