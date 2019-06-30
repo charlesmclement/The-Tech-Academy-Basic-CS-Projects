0db6319fe949dfe15dd8024338a133a6802b64ee
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-06-29 23:20:57 -0700

    Add files via upload

---
+++ stringreverse/stringreverse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace stringreverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "asdasdasd";
+            Console.Write(str + " reversed is ");
+            str = StringReverse(str);
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+        static string StringReverse(string str)
+        {
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
