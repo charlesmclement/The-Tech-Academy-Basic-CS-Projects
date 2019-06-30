76c07fe9b9fe7a5383df73adb9575212945fec61
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-06-29 23:11:22 -0700

    Add files via upload

---
+++ longint/longint/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace longint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 123456789, 123456789, 123456789, 123456789, 123456789, 123456789, 123456789, 123456789 };
+            long result = Long(arr);
+            Console.WriteLine("= " + result);
+            Console.ReadLine();
+        }
+        static long Long(int[] arr)
+        {
+            long n = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i] + " +");
+                n += arr[i];
+            }
+            return n;
+        }
+    }
+}
