4d1677e9db9b5071dbfc9d2a4e3421bab824b2f8
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-06-29 23:31:35 -0700

    Add files via upload

---
+++ removerepeats/removerepeats/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace removerepeats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "aabbccddeeffgg";
+            RemoveRepeats(str);
+            Console.ReadLine();
+        }
+
+        static string RemoveRepeats(string str)
+        {
+            Console.WriteLine(str);
+            var removed = new HashSet<char>(str);
+            Console.Write("Removed repeating characters: ");
+            foreach (char c in removed)
+                Console.Write(c);
+            return str;
+        }
+    }
+}
